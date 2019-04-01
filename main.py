07cbb25c549d793ffbec5bffb2349c2c177942c0
Utkarsh Paliwal <utkarshpaliwal@github.com>
2019-04-01 19:31:21 +0530

    Working model
    Run the main.py file to execute the program.

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 """
 @author: Utkarsh
 """
-
+import os
+os.chdir("C:/Users/alien/Documents/GitHub/Crimi-catcher")
 import speech_to_text as st
 import tense
+import temp_tense
+import is_criminal
 
 
 while True:
@@ -12,6 +15,14 @@ while True:
     if sent != -1:
         break
 print("Now let's decode")
-info = tense.getInfo(sent)
-print(*info)
-print(sent)
+if len(sent) != 0:
+    print(sent)
+else:
+    print("Nothing")
+
+info2 = temp_tense.getInfo(sent)
+
+if isCriminal(sent,info) == True:
+    print("Alert! Caution!\nThis might be a conversation to look into")
+else:
+    print("All good in here")
